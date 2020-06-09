56be6275bf0740b94bef0ae424f118822e27d1d1
Ashish kumar panda <60609102+Ashishkumarpanda@users.noreply.github.com>
2020-06-09 21:14:30 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.musicplayer;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Thread thread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    sleep(1 * 1000);//todo
+                    startActivity(new Intent(MainActivity.this, Splash.class));
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        thread.start();
+    }
+}
